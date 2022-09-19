d584066e95d6e7bf0e5139f57868c22af97a0407
Jay Edwards <j3sh0n3dward5@gmail.com>
2022-09-19 14:07:04 -0500

    updated header

---
+++ components/Home.js
@@ -0,0 +1,13 @@
+//import React from 'react'
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title> Home Page </title>
+      </Head>
+
+    </div>
+  )
+}
